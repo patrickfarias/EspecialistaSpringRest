1de49abee0a88143c292045960299afa2a995c46
“patrickfarias” <patrickfarias@icloud.com>
2020-12-21 23:16:40 -0300

    Classes Cozinha

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.repository.CozinhaRepository;
+
+import java.util.List;
+
+public class CozinhaRepositoryImpl  implements CozinhaRepository {
+    @Override
+    public List<Cozinha> listar() {
+        return null;
+    }
+
+    @Override
+    public Cozinha buscar(Long id) {
+        return null;
+    }
+
+    @Override
+    public Cozinha salvar(Cozinha cozinha) {
+        return null;
+    }
+
+    @Override
+    public void remover(Cozinha cozinha) {
+
+    }
+}
